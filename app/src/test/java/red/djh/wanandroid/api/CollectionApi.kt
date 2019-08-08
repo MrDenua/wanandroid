0c4cb25f03bd7bd8c7afb47739e51b1bbdb95461
DENUA <denua@qq.com>
2019-08-08 23:09:20 +0800

    add: test

---
+++ app/src/test/java/red/djh/wanandroid/api/CollectionApi.kt
@@ -0,0 +1,20 @@
+package red.djh.wanandroid.api
+
+import org.junit.Test
+
+/**
+ * red.djh.wanandroid.api
+ *
+ *
+ * @author dengzi
+ * @email denua@foxmail.com
+ * @date 2019/8/8
+ */
+
+class CollectionApi{
+
+    @Test
+    fun collectionListTest(){
+
+    }
+}
