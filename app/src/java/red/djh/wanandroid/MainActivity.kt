c54acec88f5ac62b7761429d6575ef9a0a9c5f29
DENUA <denua@qq.com>
2019-08-29 23:41:15 +0800

    add:navigation

--- app/src/java/red/djh/wanandroid/MainActivity.kt
+++ app/src/java/red/djh/wanandroid/MainActivity.kt
@@ -2,13 +2,19 @@ package red.djh.wanandroid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import red.djh.wanandroid.view.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var fragment:Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         LoginActivity.start(this)
+
     }
 }
